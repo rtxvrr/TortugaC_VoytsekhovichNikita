29cfd3da55eae7b2357ac062f76f7a7538bd2558
Tanatozzz Tanatozzz <ubernikita999@gmail.com>
2023-01-09 15:59:36 +0300

    unittests

--- TortugaCTestProject/UnitTest1.cs
+++ TortugaCTestProject/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using TortugaC_VoytsekhovichNikita.ClassHepler;
 
 namespace TortugaCTestProject
 {
@@ -7,8 +9,128 @@ namespace TortugaCTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TotalPrice_TrueDataOneProduct_True()
         {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = 100;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            products.Add(prd);
+            string RealResult = "100";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
+        }
+        [TestMethod]
+        public void TotalPrice_FalseDataOneProduct_False()
+        {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = -0;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            products.Add(prd);
+            string RealResult = "0";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
+        }
+        [TestMethod]
+        public void TotalPrice_NoDataNoProducts_True()
+        {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = 100;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            //products.Add(prd);
+            string RealResult = "0";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
+        }
+        [TestMethod]
+        public void TotalPrice_ZeroPriceOneProduct_True()
+        {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = 0;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            products.Add(prd);
+            string RealResult = "0";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
+        }
+        [TestMethod]
+        public void TotalPrice_MinusPriceOneProd_True()
+        {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = -100;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            products.Add(prd);
+            string RealResult = "0";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
+        }
+        [TestMethod]
+        public void TotalPrice_TrueDataTwoProduct_True()
+        {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            TortugaC_VoytsekhovichNikita.EF.Product prd2 = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = 100;
+            prd2.Price = 150;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            products.Add(prd);
+            products.Add(prd2);
+            string RealResult = "250";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
+        }
+        [TestMethod]
+        public void TotalPrice_OnePriceGoodOnePriceMinusTwoProduct_True()
+        {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            TortugaC_VoytsekhovichNikita.EF.Product prd2 = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = 100;
+            prd2.Price = -50;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            products.Add(prd);
+            products.Add(prd2);
+            string RealResult = "50";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
+        }
+        [TestMethod]
+        public void TotalPrice_TwoPriceMinusOneGoodThreeProduct_True()
+        {
+            //avg
+            TortugaC_VoytsekhovichNikita.EF.Product prd = new TortugaC_VoytsekhovichNikita.EF.Product();
+            TortugaC_VoytsekhovichNikita.EF.Product prd2 = new TortugaC_VoytsekhovichNikita.EF.Product();
+            TortugaC_VoytsekhovichNikita.EF.Product prd3 = new TortugaC_VoytsekhovichNikita.EF.Product();
+            prd.Price = -50;
+            prd2.Price = 100;
+            prd2.Price = -51;
+            List<TortugaC_VoytsekhovichNikita.EF.Product> products = new List<TortugaC_VoytsekhovichNikita.EF.Product>();
+            products.Add(prd);
+            products.Add(prd2);
+            products.Add(prd3);
+            string RealResult = "0";
+            //act
+            string FactialResult = WhiteSaturday.totalPrice(products);
+            //assert
+            Assert.AreEqual(RealResult, FactialResult);
         }
     }
 }
--- TortugaC_VoytsekhovichNikita/ClassHepler/WhiteSaturday.cs
+++ TortugaC_VoytsekhovichNikita/ClassHepler/WhiteSaturday.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TortugaC_VoytsekhovichNikita.EF;
 
 namespace TortugaC_VoytsekhovichNikita.ClassHepler
 {
-    internal class WhiteSaturday
+    public class WhiteSaturday
     {
-        public static string totalPrice(List<EF.Product> products)
+        public static string totalPrice(List<Product> products)
         {
             decimal totalCost = 0;
-            foreach (EF.Product prod in products)
+            foreach (Product prod in products)
             {
                 totalCost += prod.Price;
             }
 
-            // акция "Белая суббота".
+            // акция "Белая суббота"
             if ((DateTime.Now.Day == 29 || DateTime.Now.Day == 30 || DateTime.Now.Day == 31) && DateTime.Now.DayOfWeek.ToString() == "Saturday")
             {
                 totalCost = totalCost - (totalCost * Convert.ToDecimal(0.11));
+            }
+
+            if (totalCost < 0)
+            {
+                totalCost = 0;
             }
             return Convert.ToString(totalCost);
         }
